74dd4c9a2800dc02cb5a0b8e0a646e67c618288b
satyaveer1994 <satyaveerjat1994@gmail.com>
2022-10-09 21:43:09 +0530

    change

--- src/index.js
+++ src/index.js
@@ -33,21 +33,3 @@ app.use('/', route)
 app.listen(process.env.PORT || 3000, function() {
     console.log("Express app is running on port " + (process.env.PORT || 3000))
 })
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// mongoose
-//   .connect(
-//     "mongodb+srv://Satyaveer1994:Satyaveer123@cluster0.pn1nk.mongodb.net/satyaveer-DB",
-//     {
-//       useNewUrlParser: true,
-//     }
-//   )
-//   .then(() => console.log("MongoDb is connected"))
-//   .catch((err) => console.log(err));
-
-// app.use("/", route);
-
-// app.listen(process.env.PORT || 3000, function () {
-//   console.log("Express app running on port " + (process.env.PORT || 3000));
-// });
